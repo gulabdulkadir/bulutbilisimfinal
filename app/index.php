d5fe955a52ab1d453feaeb32a40bd83c2653a716
gulabdulkadir <abdulkadirgul6060@gmail.com>
2026-05-19 22:23:13 +0300

    bozuk surum rollingback denemesi

--- app/index.php
+++ app/index.php
@@ -63,7 +63,7 @@ if ($gelenIlce != "" && $gelenIlac != "") {
 
     <header class="hero-section">
         <div class="hero-content">
-            <h1>İlacınız Hangi Eczanede?</h1>
+            <h1>İlacınız Hangi Eczanede? - HATA VAR BOZUK SURUM</h1>
             <p>Konumunuzu seçin, aradığınız ilacın en yakın hangi eczanede olduğunu hemen bulun.</p>
 
             <form action="index.php" method="GET" class="search-wrapper" style="display: flex; align-items: center; justify-content: space-between; padding: 8px; background: white; border-radius: 50px; box-shadow: 0 10px 25px rgba(0,0,0,0.1);">
